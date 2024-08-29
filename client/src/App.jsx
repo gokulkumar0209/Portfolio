6eb4f5c54ddaa7965d96f070f4f7ddeeb97ffea6
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-08-29 16:06:55 +0530

    Added welcome component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Home from "./components/Home";
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -9,7 +10,13 @@ function App() {
 				<title>YYY</title>
 			</head>
 			<body className="bg-black min-h-screen">
-				<header className="bg-gray-800 text-white font-bold">G</header>
+				<header className=" bg-blue-900 text-white font-bold p-4 text-xl flex justify-between">
+					<h1>G</h1>
+					<div>
+						<h2>Links</h2>
+					</div>
+				</header>
+				<Home />
 			</body>
 		</>
 	);
---
+++ client/src/ui/Welcome.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../styles/welcome.css";
+function Welcome() {
+	return (
+		<div className="p-6 max-w-md mx-auto rounded-xl shadow-lg ring-1 ring-gray-900/5">
+			<div class="wrapper">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="600"
+					height="200"
+					viewBox="0 0 1300 399"
+				>
+					<text class="cls-1" transform="translate(6 271)">
+						<tspan x="0" y="0">
+							Welcome
+						</tspan>
+					</text>
+				</svg>
+			</div>
+		</div>
+	);
+}
+
+export default Welcome;
