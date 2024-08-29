f126f85666d7e7115821d8e1e83cb4c6582de5f1
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-08-29 13:52:56 +0530

    first commit initialization of project

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<head>
+				<title>YYY</title>
+			</head>
+			<body className="bg-black min-h-screen">
+				<header className="bg-gray-800 text-white font-bold">G</header>
+			</body>
+		</>
+	);
+}
+
+export default App;
