c4239e8d979b80a7470970e392c1296969ce421d
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-09-02 22:17:28 +0530

    Updated the design

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,8 +3,9 @@ import React from "react";
 import TextDesign from "../ui/TextDesign";
 function Home() {
 	return (
-		<div>
-			<TextDesign text={"Gokulkumar NT"} width={600} height={200} />
+		<div className=" flex flex-col items-center p-10  box-border">
+			<TextDesign text={"Gokulkumar NT"} width={"80vw"} height={"20vh"} />
+			<TextDesign text={"Frontend Developer"} width={"50vw"} height={"10vh"} />
 		</div>
 	);
 }
--- client/src/ui/TextDesign.jsx
+++ client/src/ui/TextDesign.jsx
@@ -3,24 +3,23 @@ import "../styles/textDesign.css"; // Ensure this file contains the CSS styles
 
 function TextDesign({ text, width = "100%", height = "100%" }) {
   return (
-    <div className="text-design-container flex justify-center items-center bg-slate-600">
-      <div className="wrapper">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width={width}
-          height={height}
-          viewBox="0 0 1300 400"
-          aria-labelledby="svgTitle"
-          role="img"
+    <div className="text-design-container">
+      <svg
+        width={width}
+        height={height}
+        viewBox="0 0 1000 100" 
+        preserveAspectRatio=" xMidYMid meet"
+      >
+        <text
+          className="text-animation"
+          x="50%"
+          y="50%"
+          dominantBaseline="middle"
+          textAnchor="middle"
         >
-          <title id="svgTitle">{text}</title>
-          <text className="text-animation" transform="translate(6 271)">
-            <tspan x="0" y="0">
-              {text}
-            </tspan>
-          </text>
-        </svg>
-      </div>
+          {text}
+        </text>
+      </svg>
     </div>
   );
 }
