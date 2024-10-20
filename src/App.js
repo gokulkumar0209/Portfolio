743769822c0271f4dcad71afcc137c6121ca04d6
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-10-20 23:58:10 +0530

    Removed states from App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Contact from "./components/Contact";
 import DotGroup from "./components/DotGroup";
 import Footer from "./components/Footer";
@@ -7,38 +6,19 @@ import LineGradient from "./components/LineGradient";
 import MySkills from "./components/MySkills";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
-import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
-	// const [selectedPage, setSelectedPage] = useState("home");
-	const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
-	// const [isTopOfPage, setIsTopOfPage] = useState(true);
-
-	// useEffect(() => {
-	// 	// eslint-disable-next-line
-	// 	const handleScroll = () => {
-	// 		if (window.scrollY === 0) {
-	// 			setIsTopOfPage(true);
-	// 		}
-	// 		if (window.scrollY !== 0) {
-	// 			setIsTopOfPage(false);
-	// 		}
-	// 		window.addEventListener("scroll", handleScroll);
-	// 		return () => window.removeEventListener("scroll", handleScroll);
-	// 	};
-	// }, []);
-
 	return (
-		<div className="app bg-deep-blue">
+		<main className="app bg-deep-blue">
 			<Navbar />
-			<Landing/>
+			<Landing />
 			<LineGradient />
 			<MySkills />
 			<Projects />
 			<LineGradient />
 			<Contact />
 			<Footer />
-		</div>
+		</main>
 	);
 }
 
