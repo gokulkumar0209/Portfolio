a89c16abba19fd822dbf314903403e8effa418a7
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-10-22 16:06:41 +0530

    Add Projects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,6 +14,7 @@ function Navbar() {
 			smooth
 			duration={1000}
 			activeClass="text-yellow"
+			offset={-100}
 		>
 			{title}
 		</LinkScroll>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,93 +1,70 @@
-import { motion } from "framer-motion";
-import LineGradient from "./LineGradient";
-import projects from "../assets/Project/projectArray";
+import React from "react";
 
-const container = {
-	hidden: {},
-	visible: {
-		transition: {
-			staggerChildren: 0.2,
-		},
-	},
-};
-
-const projectVariate = {
-	hidden: { opacity: 0, scale: 0.8 },
-	visible: { opacity: 1, scale: 1 },
-};
-
-const ProjectGif = ({ title }) => {
-	const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
-	// const projectTitle = title.split(' ');
-	console.log(title);
-	return (
-		<a target="_blank" href={title.link} rel="noreferrer">
-			<motion.div variants={projectVariate} className="relative my-2">
-				<div className={overlayStyles}>
-					<p className="text-2xl font-playfair">{title.name}</p>
-					<p className="mt-7">{title.description}</p>
-				</div>
-				<div className="flex justify-center ">
-					<img
-						src={`../assets/${title.name}.gif`}
-						className="bg-cover h-72 bg-center"
-						alt={`${title.name} img`}
-					/>
-				</div>
-			</motion.div>
-		</a>
-	);
-};
 function Projects() {
-	console.log(projects);
 	return (
-		<section id="projects" name="projects" className="pt-48 pb-48 ">
-			<motion.div
-				className="md:w-2/4 mx-auto text-center"
-				initial="hidden"
-				whileInView="visible"
-				viewport={{ once: true, amount: 0.5 }}
-				transition={{ duration: 0.5 }}
-				variants={{
-					hidden: { opacity: 0, y: -50 },
-					visible: { opacity: 1, y: 0 },
-				}}
-			>
-				<div>
-					<p className="font-playfair font-semibold text-4xl ">
-						MY <span className="text-red">PRO</span>JECTS
-					</p>
-					<div className="flex justify-center mt-5">
-						<LineGradient width="w-2/3" />
-					</div>
-				</div>
-				<p className="mt mb-10 ">
-					MyProjects provides an easy and secure way to manage project files,
-					share this information with project stakeholders, and maintain a
-					library of frequently
-				</p>
-			</motion.div>
-
-			{/* {PROJECTS} */}
-			<div className="flex justify-center">
-				<motion.div
-					className="sm:grid sm:grid-cols-3 "
-					variants={container}
-					initial="hidden"
-					whileInView="visible"
-					viewport={{ once: true, amount: 0.2 }}
+		<section className="mx-20 my-40 " name="projects">
+			<h3 className="text-5xl font-playfair z-10 text-center md:text-start text-yellow mb-40">
+				My Projects
+			</h3>
+			<div className="lg:grid lg:grid-cols-3 gap-6">
+				{/* Project 1 */}
+				<a
+					href="https://reel-saas.netlify.app/"
+					target="_blank"
+					rel="noreferrer"
 				>
-					{/* Row ONE */}
-					<div className="flex justify-center text-center items-center p-10 bg-red max-w-[580px] max-h-[400px]  text-2xl font-playfair font-semibold">
-						BEAUTIFUL USER INTERFACES
+					<div className="relative group shadow-lg rounded-lg overflow-hidden transform transition duration-500 hover:scale-105">
+						<img
+							className="w-full h-full object-cover group-hover:opacity-75 transition duration-500"
+							src="/projects/reel.png"
+							alt="Project Reel"
+						/>
+						<div className="absolute inset-0 bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 flex items-center justify-center transition duration-500">
+							<p className="text-teal-300 text-2xl font-bold">
+								Reel - Saas Landing Page
+							</p>
+						</div>
 					</div>
-					{projects?.map((item, i) => (
-						<ProjectGif title={item} key={i} />
-					))}
-					<div className="flex justify-center text-center items-center p-10 bg-yellow max-w-[580px] max-h-[400px]  text-2xl font-playfair font-semibold">
-						SMOOTH USER EXPERIENCE
+				</a>
+				{/* Project 2 */}
+				<a
+					href="https://chevy-qtrip.netlify.app/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<div className="relative group shadow-lg rounded-lg overflow-hidden transform transition duration-500 hover:scale-105">
+						<img
+							className="w-full h-full object-cover group-hover:opacity-75 transition duration-500"
+							src="/projects/qtrip.png"
+							alt="Project Qtrip"
+						/>
+						<div className="absolute inset-0 bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 flex items-center justify-center transition duration-500">
+							<p className="text-teal-300 text-2xl font-bold">
+								QTrip - Travel Booking
+							</p>
+						</div>
 					</div>
-				</motion.div>
+				</a>
+
+				{/* Project 3 */}
+				<a
+					href="https://chevy-webflix.netlify.app/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<div className="relative group shadow-lg rounded-lg overflow-hidden transform transition duration-500 hover:scale-105">
+						<img
+							className="w-full h-full object-cover group-hover:opacity-75 transition duration-500"
+							src="/projects/webflix.png"
+							alt="Project Webflix"
+						/>
+						<div className="absolute inset-0 bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 flex items-center justify-center transition duration-500">
+							<p className="text-teal-300 text-2xl font-bold">
+								Webflix - Movie Platform
+							</p>
+						</div>
+					</div>
+				</a>
 			</div>
 		</section>
 	);
